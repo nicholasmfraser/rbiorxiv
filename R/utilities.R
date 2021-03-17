4e574c53c06f92c037e40919ef1b31c92b286cbe
Nicholas Fraser <nicholasmfraser@gmail.com>
2021-03-17 11:27:58 +0100

    Improvements to error handling

--- R/utilities.R
+++ R/utilities.R
@@ -3,46 +3,63 @@ base_url <- function() {
   return("https://api.biorxiv.org")
 }
 
-# Make query to API and return full response content
-fetch_content <- function(url) {
-  request <- httr::RETRY(verb = "GET",
-                         url = url,
-                         times = 5,
-                         pause_base = 1,
-                         pause_cap = 60,
-                         httr::timeout(60))
-  handle_response(request = request)
-  content <- httr::content(request, as = "parsed")
-  Sys.sleep(1) # Add a 1-second pause between each API call
-  return(content)
-}
-
 # Check internet connection
 check_internet_connection <- function() {
   if (curl::has_internet() == FALSE) {
     stop("No internet connection detected. ",
          "Please connect to the internet and try again.",
          call. = F)
-  return(invisible(NULL))
   }
 }
 
-# Handle HTTP responses
+# Make query to API and return full response content
+fetch_content <- function(url) {
+  request <- httr::RETRY(verb = "GET",
+                         url = url,
+                         pause_base = 1,
+                         pause_cap = 60,
+                         times = 5,
+                         quiet = TRUE,
+                         httr::timeout(60))
+  content <- handle_response(request = request)
+  return(content)
+}
+
+# Handle http responses
+# In addition to typical http responses, the following two cases may occur:
+# 1. A 200 status code may be received, but the response body contains a message
+# that the connection is refused (probably due to high server loads). In this
+# case an error is thrown and the action is stopped.
+# 2. A 200 status code is received, bu the response body contains a status
+# stating that no results were returned, either because no results are expected
+# or due to discrepancies between the expected and actual number of results
+# returned by the API, which interferes with paging functions. In this case a
+# null response is returned to be handled by the individual API endpoint functions.
 handle_response <- function(request) {
   if (request$status_code == 200) {
-    message <- httr::content(request, as = "text", encoding = "UTF-8")
-    if (message == "Error : (2002) Connection refused") {
-      stop("Connection refused. The server may be experiencing high loads.",
-           " Please try again later.", call. = F)
+    if (check_refused_connection(request)) {
+      stop("Connection refused. The server may be experiencing high loads. ",
+           "Please try again later.", call. = F)
+    } else if (check_empty_response(request)) {
+      content <- NULL
+    } else {
+      content <- httr::content(request, as = "parsed")
     }
-    status <- unlist(httr::content(request, as = "parsed")$messages)["status"]
-    if (status == "no articles found") {
-      stop("No articles found. ",
-           "Please adjust query parameters and try again.",
-           call. = F)
-    }
+    return(content)
   }
   httr::stop_for_status(request)
+}
+
+# Check if a "Connection refused" message is returned
+check_refused_connection <- function(request) {
+  message <- httr::content(request, as = "text", encoding = "UTF-8")
+  return(message == "Error : (2002) Connection refused")
+}
+
+# Check if a "no posts found" status is returned
+check_empty_response <- function(request) {
+  status <- unlist(httr::content(request, as = "parsed")$messages)["status"]
+  return(status == "no posts found")
 }
 
 # Return the data in the requested format
